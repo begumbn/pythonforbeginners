d76a213889696117bba3858421c49a41076f66f2
begumbn <begumbidik@gmail.com>
2015-07-22 22:51:44 +0100

    Create 3-1.py

---
+++ 3-1.py
@@ -0,0 +1,13 @@
+# pay rate calculator with overpay option
+
+hrs = raw_input("Enter Hours:")
+h = float(hrs)
+rate= raw_input("Enter Rate:")
+r = float(rate)
+
+if h<40 :
+    pay = h*r
+else :
+    pay = (40 * r) + ((h%40) * (r*1.5))
+
+print pay
